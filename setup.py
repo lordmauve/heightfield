a9f084f361ea64832a4e916cf6ad8f919f596c79
Daniel Pope <ext-dan.pope@nokia.com>
2012-02-02 16:32:55 +0000

    Fix a few packaging problems

--- setup.py
+++ setup.py
@@ -2,17 +2,25 @@ import os
 from setuptools import setup, find_packages
 
 # Load long_description from README.rst
-readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+readme = os.path.join(os.path.dirname(__file__), 'README')
 long_description = open(readme, 'rU').read().decode('utf8')
+
+# Construct version string
+from heightfield import __version__
+version = '.'.join([str(component) for component in __version__])
 
 setup(
     name='heightfield',
-    version="0.1",
+    version=version,
     packages=find_packages(),
+    package_data={
+        'heightfield': ['data/*.png'],
+    },
     description=u"Generate random maps by particle deposition",
     long_description=long_description,
     author='Daniel Pope / Reading Python Dojo',
     author_email='lord.mauve@gmail.com',
+    url='https://bitbucket.org/lordmauve/heightfield',
     install_requires=[
         'PIL>=1.1.6',
     ],
@@ -25,9 +33,6 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Multimedia :: Graphics',
     ],
-    package_data={
-        'heightfield': ['data/*.png'],
-    },
     entry_points={
         'console_scripts': [
             'heightfield = heightfield.main:main',
